188eaedf31ffc11d0f030c2efa643edc352e063d
garry014 <66904472+garry014@users.noreply.github.com>
2020-12-22 09:38:31 +0800

    revert

--- __init__.py
+++ __init__.py
@@ -17,16 +17,23 @@ def manage_ads():
 
 @app.route('/advertise', methods=['GET', 'POST'])
 def advertise():
-    error = None
     create_ad = CreateAd(request.form)
+<<<<<<< HEAD
     if request.method == 'POST' and create_ad.validate():
         if (create_ad.startdate.data > create_ad.enddate.data): #Compare start and end dates.
             error = "End date cannot be earlier than start date"
+=======
+    if request.method == 'POST' and create_ad.validate(): #
+        print(create_ad.startdate.data)
+        CreateAd().compare_dates()
+        if (create_ad.startdate.data > create_ad.enddate.data):
+            pass
+>>>>>>> parent of b486dc6... wtform sucks
         else:
             print("This is running")
             return redirect(url_for('home'))
 
-    return render_template('advertise.html', form=create_ad, error = error)
+    return render_template('advertise.html', form=create_ad)
 
 #ERROR 404 Page
 @app.errorhandler(404)
