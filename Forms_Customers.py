58d66a1158b8e52666cf50e50f8fc37b9eee7a4b
sekki2323 <73709725+sekki2323@users.noreply.github.com>
2021-01-27 11:57:22 +0800

    Add files via upload

---
+++ Forms_Customers.py
@@ -0,0 +1,16 @@
+from wtforms import Form, StringField, SelectField, validators
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, Regexp, Length
+
+
+class Customer_Update(Form):
+    firstname = StringField('Firstname', [validators.Length(min=1, max=100), validators.DataRequired()])
+    lastname = StringField('Lastname', [validators.Length(min=1, max=100), validators.DataRequired()])
+    address1 = StringField("Address Line 1", [validators.DataRequired()])
+    address2 = StringField("Address Line 2", [validators.DataRequired()])
+    city = StringField("City", [validators.DataRequired()])
+    postal_code = StringField('Postal Code', [Length(6), validators.DataRequired()])
+    email = EmailField('Email',  validators=[InputRequired("Please enter your email address."), validators.Email("Please enter your email address.")])
+    phone_number = StringField('Phone Number', [Length(8), validators.DataRequired()])
+    password = StringField('Password (minimum eight characters, at least one letter, one number and one special character) ', [validators.DataRequired(), Regexp('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$')])
+
