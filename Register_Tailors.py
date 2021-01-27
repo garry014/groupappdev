29c59e22e0ca90dced5230aa284ea6684478ea89
sekki2323 <73709725+sekki2323@users.noreply.github.com>
2021-01-27 11:49:30 +0800

    Add files via upload

---
+++ Register_Tailors.py
@@ -0,0 +1,18 @@
+from wtforms import Form, StringField, validators
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, Regexp, Length
+
+
+class RegisterTailors(Form):
+    firstname = StringField('Firstname', [validators.Length(min=1, max=100), validators.DataRequired()])
+    lastname = StringField('Lastname', [validators.Length(min=1, max=100), validators.DataRequired()])
+    user_name = StringField('Username (minimum 6 characters)', [validators.Length(min=6, max=30), validators.DataRequired()])
+    password = StringField('Password (minimum eight characters, at least one letter, one number and one special character) ', [validators.DataRequired(), Regexp('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$')])
+    store_name = StringField("Store Name", [validators.DataRequired()])
+    address1 = StringField("Address Line 1", [validators.DataRequired()])
+    address2 = StringField("Address Line 2", [validators.DataRequired()])
+    city = StringField("City", [validators.DataRequired()])
+    postal_code = StringField('Postal Code', [Length(6), validators.DataRequired()])
+    email = EmailField('Email',  validators=[InputRequired("Please enter your email address."), validators.Email("Please enter your email address.")])
+    phone_number = StringField('Phone Number', [Length(8), validators.DataRequired()])
+
